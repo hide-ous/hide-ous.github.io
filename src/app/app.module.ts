ff546b35abefa80fd1dced846c33c89e8d7d9f32
hide-ous <mattia.samory@gmail.com>
2021-11-02 18:58:41 +0100

    + medallion component and stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PaperComponent } from './paper/paper.component';
 import { PapersComponent } from './papers/papers.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MedallionComponent } from './medallion/medallion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaperComponent,
     PapersComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    MedallionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/medallion/medallion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medallion',
+  templateUrl: './medallion.component.html',
+  styleUrls: ['./medallion.component.css']
+})
+export class MedallionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
